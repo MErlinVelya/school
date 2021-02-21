52c27e3203c66650d62ceae29082c0150328c71e
MErlin <Me@example.com>
2021-02-21 23:16:16 +0200

    working

--- src/main/java/SchoolConsoleApplication.java
+++ src/main/java/SchoolConsoleApplication.java
@@ -23,19 +23,19 @@ public class SchoolConsoleApplication {
 //
 //        String[] groups = contentProvider.generateGroups(10);
 //        System.out.println(groups[9]);
-//        DatabaseConnector databaseConnector = new DatabaseConnector();
+        DatabaseConnector databaseConnector = new DatabaseConnector();
 //
-//        DatabiseFiller databiseFiller = new DatabiseFiller();
+        DatabiseFiller databiseFiller = new DatabiseFiller();
 //
-//           // Connection con = databaseConnector.connect("jdbc:postgresql://localhost:5433/school", "postgres", "admin");
-//        Connection con = databaseConnector.connect("jdbc:postgresql://localhost:5432/school", "merlin", "");
+//        Connection con = databaseConnector.connect("jdbc:postgresql://localhost:5433/school", "postgres", "admin");
+        Connection con = databaseConnector.connect("jdbc:postgresql://localhost:5432/school", "merlin", "");
 
-        //databiseFiller.createTables(con);
+        databiseFiller.createTables(con);
       //databiseFiller.fillTables(con, contentProvider.generateGroups(10), contentProvider.getCourses(), contentProvider.generateStudents(200));
 
 
         List<Student> students = contentProvider.generateStudentsAllIn(10,30,1,3,200);
-        System.out.println(students.toString());
+        //System.out.println(students.toString());
 
 
     }
--- src/main/java/com/veresklia/dao/DatabiseFiller.java
+++ src/main/java/com/veresklia/dao/DatabiseFiller.java
@@ -12,9 +12,12 @@ public class DatabiseFiller {
         String createTables = new StringBuilder()
             .append("DROP TABLE IF EXISTS groups, students, courses, enrollments;")
             .append("CREATE TABLE groups (group_id SERIAL PRIMARY KEY, group_name varchar);")
-            .append("CREATE TABLE students (student_id SERIAL PRIMARY KEY, group_id int, first_name varchar, last_name varchar);")
+            .append("CREATE TABLE students (student_id SERIAL PRIMARY KEY, group_id int, first_name varchar, last_name varchar, ")
+            .append("FOREIGN KEY (group_id) REFERENCES groups(group_id) ON DELETE SET NULL);")
             .append("CREATE TABLE courses (course_id SERIAL PRIMARY KEY, course_name varchar, course_description varchar);")
-            .append("CREATE TABLE enrollments (enrollment_id SERIAL PRIMARY KEY, course_id int, student_id int);")
+            .append("CREATE TABLE enrollments (enrollment_id SERIAL PRIMARY KEY, course_id int NOT NULL, student_id int NOT NULL, ")
+            .append("FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE, ")
+            .append("FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE);")
             .toString();
 
         try (Statement statement = connection.createStatement()) {
