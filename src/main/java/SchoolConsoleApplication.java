33f95d95433f26b5f213eecad76d43d742747e5f
MErlin <Me@example.com>
2021-02-27 16:46:57 +0200

    working

--- src/main/java/SchoolConsoleApplication.java
+++ src/main/java/SchoolConsoleApplication.java
@@ -2,11 +2,15 @@
 
 import com.veresklia.dao.connector.DatabaseConnector;
 import com.veresklia.domain.Group;
+import com.veresklia.domain.Student;
 import com.veresklia.domain.provider.ContentProvider;
 import com.veresklia.dao.DatabiseFiller;
+import com.veresklia.domain.provider.CoursesProvider;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SchoolConsoleApplication {
 
@@ -14,12 +18,13 @@ public class SchoolConsoleApplication {
         ContentProvider contentProvider = new ContentProvider();
         DatabaseConnector databaseConnector = new DatabaseConnector();
         DatabiseFiller databiseFiller = new DatabiseFiller();
-    Connection con = databaseConnector.connect("jdbc:postgresql://localhost:5433/school", "postgres", "admin");
-  //      Connection con = databaseConnector.connect("jdbc:postgresql://localhost:5432/school", "merlin", "");
+//
+        //      Connection con = databaseConnector.connect("jdbc:postgresql://localhost:5433/school", "postgres", "admin");
+        Connection con = databaseConnector.connect("jdbc:postgresql://localhost:5432/school", "merlin", "");
 
         databiseFiller.createTables(con);
         Group[] groups = contentProvider.generateGroups(10, 10, 30);
 
-        databiseFiller.fillTables(con, groups, contentProvider.getCourses(), contentProvider.generateStudentsAllIn(groups, 1, 3, 200));
+        databiseFiller.fillTables(con, groups, CoursesProvider.getCourses(), contentProvider.generateStudentsAllIn(groups, 1, 3, 200));
     }
 }
